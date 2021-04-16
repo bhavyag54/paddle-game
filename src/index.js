6f7125b88cddc3c63d3afbdecf6c60bbedba17df
bhavya <bhavyagoyal54@gmail.com>
2021-04-16 22:28:43 +0530

    changed import paths

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Paddle from './paddle.js';
-import InputHandler from './input.js';
-import Ball from './Ball.js';
+import Paddle from '/src/paddle.js';
+import InputHandler from '/src/input.js';
+import Ball from '/src/Ball.js';
 
 let canvas = document.getElementById('gameScreen');
 
